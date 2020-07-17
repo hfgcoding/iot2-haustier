a571eb9145b20e038ce7b7aa954fecfb3c027a49
Florian Geiselhart <florian.geiselhart@hfg.design>
2020-07-17 16:37:55 +0200

    Fix AWS server address

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ Vue.config.productionTip = false
 
 Vue.use(new VueSocketIO({
     debug: true,
-    connection: 'http://63.32.119.241:10000/floprivatbot_control', //WICHTIG: Hier nach dem Slash den Namen eures Bots angeben (wie auf dem Pi festgelegt)
+    connection: 'https://hal.hfg.design:10000/floprivatbot_control', //WICHTIG: Hier nach dem Slash den Namen eures Bots angeben (wie auf dem Pi festgelegt)
     options: {
       useConnectionNamespace: true
     }
